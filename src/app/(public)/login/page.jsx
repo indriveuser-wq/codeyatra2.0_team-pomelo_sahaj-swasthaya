adf0b6d01af1313f28f0f7feef2de750958fb0a8
Kushal Dhakal <160240786+Kushal216@users.noreply.github.com>
2026-02-22 15:52:32 +0545

    feat:login page created

---
+++ src/app/(public)/login/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
+function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+    if (data.success) login(data.token, data.user);
+    else alert(data.error);
+  };
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center bg-gray-100 rounded-4xl">
+        <form
+          onSubmit={handleSubmit}
+          className="p-8 bg-white rounded shadow-md w-96"
+        >
+          <h2 className="text-2xl mb-4 font-bold">Login</h2>
+          <input
+            className="w-full p-2 mb-4 border  rounded-xl"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            className="w-full p-2 mb-4 border  rounded-xl"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button className="w-full bg-blue-600 text-white p-2 rounded-xl">
+            Login
+          </button>
+          <p className="mt-4 text-sm text-center">
+            No account?{" "}
+            <Link href="/register" className="text-blue-500 ">
+              Don,t have an account? , Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Page;
