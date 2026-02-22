3cd1ac25bd5919aefe16a52d7b35a3934677efd0
Linus <bhattaraisunil76@gmail.com>
2026-02-22 15:28:06 +0545

    fix: bcrypt uninstalled

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { connectDB } from '@/lib/db';
 import User from '@/models/User';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 export async function POST(req) {
   try {
